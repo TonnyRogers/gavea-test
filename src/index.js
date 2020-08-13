b3af58e3b037daca16ff34891113afedf5c3ff34
tonnyrogers <antoniel15975@gmail.com>
2020-08-13 13:08:00 -0300

    App Directory Structure

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, StatusBar} from 'react-native';
+
+// import { Container } from './styles';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="#FFF" />
+      <View />
+    </>
+  );
+};
+
+export default App;
